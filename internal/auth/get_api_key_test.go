7b0417321505e540d128c0b8fdce530f137ad1ad
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2026-06-03 13:12:10 +0100

    add style check

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -2,25 +2,24 @@ package auth
 
 import (
 	"errors"
-	"testing"
 	"net/http"
+	"testing"
 )
-
 
 func TestApiKey(t *testing.T) {
 	headers := http.Header{}
 	headers.Set("Authorization", "ApiKey 123456")
 
-	result, err := GetAPIKey(headers) 
-	if err != nil { 
+	result, err := GetAPIKey(headers)
+	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	expected := "123456" 
-		if result != expected {
-			t.Errorf("expected %s, got %s", expected, result)
-		}
-	
+	expected := "123456"
+	if result != expected {
+		t.Errorf("expected %s, got %s", expected, result)
+	}
+
 }
 
 func TestEmptyHeader(t *testing.T) {
@@ -33,6 +32,6 @@ func TestEmptyHeader(t *testing.T) {
 	}
 
 	if !errors.Is(err, ErrNoAuthHeaderIncluded) {
-		 t.Errorf("expected error %v, got %v", ErrNoAuthHeaderIncluded, err)
+		t.Errorf("expected error %v, got %v", ErrNoAuthHeaderIncluded, err)
 	}
 }
