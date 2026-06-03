5d5e900196c62413ffbe6714624411163fed6edb
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2026-06-03 10:52:01 +0100

    tests, broken code

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"errors"
+	"testing"
+	"net/http"
+)
+
+
+func TestApiKey(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey 123456")
+
+	result, err := GetAPIKey(headers) 
+	if err != nil { 
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expected := "123456" 
+		if result != expected {
+			t.Errorf("expected %s, got %s", expected, result)
+		}
+	
+}
+
+func TestEmptyHeader(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "")
+
+	_, err := GetAPIKey(headers)
+	if err == nil {
+		t.Fatalf("expected error, didn't find one")
+	}
+
+	if !errors.Is(err, ErrNoAuthHeaderIncluded) {
+		 t.Errorf("expected error %v, got %v", ErrNoAuthHeaderIncluded, err)
+	}
+}
