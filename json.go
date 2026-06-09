f22cee883c142c1bcd60d2d69940be776c886845
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2026-06-09 12:32:26 +0100

    fix fmt

--- json.go
+++ json.go
@@ -32,7 +32,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 
 	w.WriteHeader(code)
 
-	if _, err:= w.Write(dat); err != nil {
+	if _, err := w.Write(dat); err != nil {
 		log.Printf("Error writing JSON response: %v", err)
 	}
 }
--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-
-	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
